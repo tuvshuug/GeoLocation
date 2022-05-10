1514077e39b40211d8d10db8659de4c028431b50
tuvshuug <52911991+tuvshuug@users.noreply.github.com>
2022-05-11 00:38:53 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import * as React from 'react';
+import MapView, { Callout, Circle, Marker } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import * as Location from 'expo-location';
 export default function App() {
+  const [pin, setPin] = React.useState({
+    latitude: 13.406,
+    longitude: 123.3753,
+  });
+
+  React.useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+      setPin({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
+    })();
+  }, []);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map}
+        
+        showsUserLocation={true}
+        onUserLocationChange={(e) =>{
+          console.log("onUserLocationChange", e.nativeEvent.coordinate);
+
+          setPin({
+            latitude: e.nativeEvent.coordinate.latitude,
+            longitude: e.nativeEvent.coordinate.longitude,
+          });// odoo baigaa barshiltai hamt setpin hiisen coordinate ni dagana
+
+
+        }}
+      >
+      </MapView>
+
     </View>
   );
 }
@@ -16,5 +54,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
